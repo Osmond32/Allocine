cd74e3c44c2b4d09a47e24ea43579b69a6aa6911
Osmond32 <saia.giuseppe@live.it>
2026-04-27 14:04:52 +0200

    case sensit

---
+++ src/Services/config.js
@@ -0,0 +1,7 @@
+export const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOGRhNzhiYzRjYzAzYTg1ODI0NmM4YTEyN2IxODRkNSIsIm5iZiI6MTc2MDYxMDAzNi44NzgsInN1YiI6IjY4ZjBjNmY0ZTQzNWViYTAyZTFlMjBkYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qpSlcJd511iXWxIJhhUynSz66QMrzDY0OMdTBUl1bO0"
+export const ACCOUNT_ID ="22388789"
+export const HEADER = {
+    headers: {
+        "Authorization" : "Bearer " + TOKEN
+    }
+}
